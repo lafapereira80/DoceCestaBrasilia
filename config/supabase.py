27cbebcfed6f00c7271ca6b46e84251510728f95
lafapereira80 <lafapereira@gmail.com>
2026-07-15 15:08:47 -0300

    Add logging for Supabase URL and KEY

--- config/supabase.py
+++ config/supabase.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from supabase import create_client
 
+st.write("URL:", st.secrets["SUPABASE_URL"])
+st.write("KEY:", st.secrets["SUPABASE_KEY"][:30])
+
 supabase = create_client(
     st.secrets["SUPABASE_URL"],
     st.secrets["SUPABASE_KEY"]
